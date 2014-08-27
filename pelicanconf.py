c81f436e6e8fb2d706cb60f61f5d64e01eeeed06
liulixiang1988 <liulixiang1988@gmail.com>
2014-08-27 15:41:18 +0800

    modify theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,4 +26,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME="pelican-themes/cebong"
+THEME="pelican-themes/SoMA"
